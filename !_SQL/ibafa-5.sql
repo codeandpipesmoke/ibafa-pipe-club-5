525229ab907bd1aa14e94f9f97388f365ef75d79
Jeff Shoemaker <codeandpipesmoke@gmail.com>
2025-05-18 15:50:55 +0200

    Daily backup Only

--- !_SQL/ibafa-5.sql
+++ !_SQL/ibafa-5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Ápr 15. 20:36
+-- Létrehozás ideje: 2025. Máj 18. 15:50
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
